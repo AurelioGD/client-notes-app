eeb83186188f7c625a9087962fb042f3f18330a9
Aurelio Gonzalez <agg156231@gmail.com>
2021-08-12 16:51:40 -0500

    Se realizaron cambios esteticos al componente de note, esta listo para empezar con la logica.

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,14 +1,17 @@
 import './Note.css'
+import starOn from '../../svg/starOn-icon.svg'
+import starOff from '../../svg/starOff-icon.svg'
 
-
-const Note = ({title = "waiting..", description="waiting..", }) => {
+const Note = ({title = "loading..", description="loading..", }) => {
     return (
         <div className="note">
+            <button className="note__star"> <img className="note__icon-star" src={starOn} alt="icon-start" /> </button>
             <h3 className="note__title">{title}</h3>
             <p className="note__description">{description}</p>
             <div className="note__actions">
                 <button className="note__btn btn-edit">Edit</button>
                 <button className="note__btn btn-about">About</button>
+                <button className="note__btn btn-delete">Delete</button>
             </div>
         </div>
     )
